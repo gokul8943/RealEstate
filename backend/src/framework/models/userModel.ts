25431b60f5ce56da11fa064e337e3f753c25d521
Gokul M R <gokulmr331@gmail.com>
2026-02-06 09:08:56 +0530

    update:realestate fixes

---
+++ backend/src/framework/models/userModel.ts
@@ -0,0 +1,9 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
